58180abc0149849123160c6cd5c7d81f713a3137
achahine78 <achahine78@gmail.com>
2023-05-28 21:21:15 +0400

    feat: add some error handling for user creation

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -2,19 +2,43 @@ import { prisma } from "../db/prisma";
 import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
 export const createUser = async (req, res) => {
+  if (!req.body.username) {
+    res.status(422);
+    res.json({ message: "Username field is missing." })
+    return;
+  }
+
+  if (!req.body.password) {
+    res.status(422);
+    res.json({ message: "Password field is missing." })
+    return;
+  }
+
+  if (!req.body.email) {
+    res.status(422);
+    res.json({ message: "Email field is missing." })
+    return;
+  }
+
   const username = req.body.username;
   const password = await hashPassword(req.body.password);
   const email = req.body.email;
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password,
-    },
-  });
 
-  const token = createJWT(user);
-  res.json({ token });
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (e) {
+    res.status(409);
+    res.json({ message: "Email or username are already taken." });
+  }
 };
 
 export const login = async (req, res) => {
