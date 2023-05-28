c0534dd81383c8f6d81bc2b4ef75fff3ee8e76f7
achahine78 <achahine78@gmail.com>
2023-05-28 19:47:55 +0400

    chore: add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
-import express from 'express';
-import router from './router';
-import { protect } from './modules/auth';
-import { createUser, login } from './handlers/user';
+import express from "express";
+import cors from "cors";
+import router from "./router";
+import { protect } from "./modules/auth";
+import { createUser, login } from "./handlers/user";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.status(200);
-    res.json({
-        message: 'success',
-    })
+app.get("/", (req, res) => {
+  res.status(200);
+  res.json({
+    message: "success",
+  });
 });
 
-app.use('/api', protect, router);
+app.use("/api", protect, router);
 
-app.post('/signup', createUser);
-app.post('/login', login);
+app.post("/signup", createUser);
+app.post("/login", login);
 
 export default app;
